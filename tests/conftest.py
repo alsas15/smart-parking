2606888198f2e758fbcf943c8ca8d568477c658f
Alexander Butko <alissanya2@gmail.com>
2025-07-25 09:06:46 +0300

    Correct homework

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Generator
 
 import pytest
@@ -6,7 +7,7 @@ from flask.testing import FlaskClient
 from sqlalchemy.orm import scoped_session
 
 from app import create_app, db
-from app.models import Client, Parking
+from app.models import Client, Parking, ClientParking
 
 
 @pytest.fixture
@@ -42,8 +43,13 @@ def setup_data(db_session: scoped_session) -> dict[str, object]:
     parking = Parking(
         address="Main Street", opened=True, count_places=10, count_available_places=5
     )
-    db_session.add(client)
-    db_session.add(parking)
+    db_session.add_all([client, parking])
+    db_session.commit()
+
+    client_parking = ClientParking(
+        client_id=client.id, parking_id=parking.id, time_in=datetime.utcnow()
+    )
+    db_session.add(client_parking)
     db_session.commit()
 
     return {"client": client, "parking": parking}
